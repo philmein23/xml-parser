a5cdaa9903ab83f02bcbbe0b5f6c993720bbe802
Phillip Nguyen <c-phillip.nguyen@hulu.com>
2020-01-30 10:26:18 -0800

    added identifier and pair parsers

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
 
     match chars.next() {
         Some(next) if next.is_alphabetic() => matched.push(next),
-        _ => return Err(input)
+        _ => return Err(input),
     }
 
     while let Some(next) = chars.next() {
@@ -41,11 +41,55 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
 #[test]
 fn literal_parser() {
     let parse_joe = match_literal("Hello Joe!");
     assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
-    assert_eq!(Ok((" Hello Robert!", ())), parse_joe("Hello Joe! Hello Robert!"));
+    assert_eq!(
+        Ok((" Hello Robert!", ())),
+        parse_joe("Hello Joe! Hello Robert!")
+    );
     assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+}
 
+#[test]
+fn identifier_parser() {
+    assert_eq!(
+        Ok(("", "i-am-an-identifier".to_string())),
+        identifier("i-am-an-identifier")
+    );
+
+    assert_eq!(
+        Ok((" entirely an identifier", "not".to_string())),
+        identifier("not entirely an identifier")
+    );
+
+    assert_eq!(
+        Err("!not at all an identifier"),
+        identifier("!not at all an identifier")
+    );
+}
+
+#[test]
+fn pair_combinator() {
+    let tag_opener = pair(match_literal("<"), identifier);
+
+    assert_eq!(
+        Ok(("/>", ((), "my-first-element".to_string()))),
+        tag_opener("<my-first-element/>")
+    )
 }
